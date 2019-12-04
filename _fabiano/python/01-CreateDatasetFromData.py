664cea38f354373c45125182d6672c61e9eebdd0
Fabiano Papaiz <fabiano.papaiz@lais.huol.ufrn.br>
2019-12-04 15:13:24 -0300

    dados extraídos; criados arquivos CSV para usar no Python; com adicao da medida k_unordered_sequence

---
+++ _fabiano/python/01-CreateDatasetFromData.py
@@ -0,0 +1,95 @@
+import sys
+import os
+import shutil
+import datetime
+import time
+
+
+def calcular_k_unordered_sequence(data):
+    arr = data.split(' ')
+    #
+    indice1 = 0
+    indice2 = 1
+    indice_max = len(arr) - 1
+    k = 0
+    #
+    while (indice1 < indice_max):
+        #
+        if (int(arr[indice1]) > int(arr[indice2])):
+            k += 1
+        #
+        indice1 += 1
+        indice2 += 1
+    #
+    return k
+
+
+
+# define as pastas contendo os dados
+pasta_origem = '../data'
+pastas = os.listdir(pasta_origem)
+
+#abre o arquivo onde serao salvos os dados extraidos
+#se o arquivo ja existir, entao apaga e cria novamente
+path_arq_destino = '%s/_dados_extraidos.csv'%pasta_origem
+if os.path.exists(path_arq_destino):
+    os.remove(path_arq_destino)
+
+arq_destino = open(path_arq_destino, 'w+')
+arq_destino.write('%s;%s;%s;%s;%s;%s\n' % ('algoritmo', 'probabilidade_erro', 'size_of_array', 'largest_sorted_subarray', 'k_unordered_sequence', 'percentual_k_unordered') )
+
+#
+for pasta_prob_erro in pastas:
+    #teste para não pegar a pasta ".DStore" do macOS
+    if (pasta_prob_erro[0] not in ['.','_']):
+        prob_erro = pasta_prob_erro.split('-')[1].replace(' div ', '/')
+        pastas_prob_erro = '%s/%s' % (pasta_origem, pasta_prob_erro)
+        pastas = os.listdir(pastas_prob_erro)
+        print('*processando: prob. erro = %s' % prob_erro)
+        #
+        for pasta_algoritmo in pastas:
+            # teste para não pegar a pasta ".DStore" do macOS
+            if (pasta_algoritmo[0] != '.'):
+                algoritmo = pasta_algoritmo
+                pastas_algoritmo = '%s/%s' % (pastas_prob_erro, pasta_algoritmo)
+                # print(pastas_prob_erro,' || ', pastas_algoritmo)
+                pastas = os.listdir(pastas_algoritmo)
+                # print('    *processando: algoritmo = %s' % algoritmo)
+                #
+                for pasta_n in pastas:
+                    # teste para não pegar a pasta ".DStore" do macOS
+                    if (pasta_n[0] != '.'):
+                        n = pasta_n
+                        pastas_n = '%s/%s' % (pastas_algoritmo, pasta_n)
+                        pastas = os.listdir(pastas_n)
+                        # print(pastas_n)
+                        # print('        *processando: n = %s' % n)
+                        #
+                        for arq_out in pastas:
+                            # print('            *processando: arq = %s' % arq_out)
+                            arq = '%s/%s' % (pastas_n, arq_out)
+                            # print(arq)
+
+                            #le o arquivo .OUT
+                            file = open(arq, 'r')
+                            #obtem os dados de cada uma das 4 linhas
+                            lines = file.readlines()
+                            l1_original_array = lines[0]
+                            l2_sorting_with_fault = lines[1]
+                            l3_sorting_ok = lines[2]
+                            l4_largest_sorted_subarray = int(lines[3])
+
+                            #se for a prob. erro default (1/n) ajusta a descricao no arquivo
+                            if (prob_erro == '1/n'):
+                                prob_erro_csv = '%g' % (1.0/int(n))
+                                # prob_erro_csv = '1/%s'% (n)
+                            else:
+                                prob_erro_csv = prob_erro
+                            #
+                            k_unordered_sequence = calcular_k_unordered_sequence(l2_sorting_with_fault)
+                            #
+                            percentual_k_unordered = 100.0 * int(k_unordered_sequence) / int(n)
+                            #
+                            arq_destino.write( '%s;%s;%s;%s;%s;%.2f\n' % (algoritmo, prob_erro_csv, n, l4_largest_sorted_subarray, k_unordered_sequence, percentual_k_unordered ))
+
+                            # break
---
+++ _fabiano/python/gen.py
@@ -0,0 +1,33 @@
+from random import *
+
+n_sizes = [100, 1000, 10000]
+#n_sizes = [1, 2, 3]
+
+for n in n_sizes:
+    #eps = 1.0/n
+    eps = 0.05
+    maxr = n/2
+    arq = "_data-005-%s.in"%(n)
+    f = open(arq,"w")
+    f.write(str(eps) + " " + str(n))
+    for i in range(n):
+        f.write(" " + str(randint(1,maxr)))
+    f.write("\n")
+    f.close()
+
+# ============================
+# CODIGO ANTERIOR
+# ============================
+# n = 10000
+# eps = 1.0/n
+# maxr = n/2
+
+# f = open("data2.in","w")
+# f.write(str(eps) + " " + str(n))
+
+# for i in range(n):
+#     f.write(" " + str(randint(1,maxr)))
+# f.write("\n")
+
+# f.close()
+
